1c39883a16515aabce8a79c2f55f596c49dd9ec3
Viram Jain <86965162+viramjain13@users.noreply.github.com>
2023-06-05 11:34:32 +0530

    Create test.py

---
+++ version control job/test.py
@@ -0,0 +1,16 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+dyf = glueContext.create_dynamic_frame.from_catalog(database='champion', table_name='transactions')
+dyf.printSchema()
+df = dyf.toDF()
+df.show()
