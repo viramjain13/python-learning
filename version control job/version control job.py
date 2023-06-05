7a4f715e993bf844f8a11d30691e5188f5f4d8bc
Viram Jain <86965162+viramjain13@users.noreply.github.com>
2023-06-05 12:36:23 +0530

    Synchronizing job testing job from AWS Glue at 2023/06/05 07:06:23

--- version control job/version control job.py
+++ version control job/version control job.py
@@ -5,12 +5,12 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
-sc = SparkContext.getOrCreate()
+sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-dyf = glueContext.create_dynamic_frame.from_catalog(database='champion', table_name='transactions')
-dyf.printSchema()
-df = dyf.toDF()
-df.show()
+job.init(args['JOB_NAME'], args)
+job.commit()
